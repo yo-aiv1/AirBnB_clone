d5a1687ab8e954a3bf709759460eb4579b3012a6
Yahya <yahyaoulha0@gmail.com>
2023-12-09 13:15:33 +0100

    fixed importing problem

--- console.py
+++ console.py
@@ -4,12 +4,12 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
-import shlex
 from models.place import Place
 from models.city import City
 from models.amenity import Amenity
 from models.state import State
 from models.review import Review
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
