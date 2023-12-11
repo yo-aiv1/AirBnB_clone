86312e9c0a5f31123c67e225fd3010f292f5674e
zakariasan <elitziko@gmail.com>
2023-12-11 22:50:32 +0100

    test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -95,6 +95,10 @@ class TestFileStorage(unittest.TestCase):
 
         all_objs = new_storage.all()
 
+    def test_file_path(self):
+        """Test the __file_path attribute."""
+        self.assertIsNone(FileStorage.__file_path)
+
 
 if __name__ == '__main__':
     unittest.main()
